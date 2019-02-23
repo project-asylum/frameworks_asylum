fc43ba405cc750d6b798abf060d424354e8c6817
Griffin Millender <griffinn.millender@gmail.com>
2019-02-23 13:27:37 -0600

    New button settings and configuration

---
+++ libs/keys/src/com/asylum/keys/parser/KeyCategory.java
@@ -0,0 +1,10 @@
+package com.asylum.keys.parser;
+
+public class KeyCategory {
+    public String name;
+    public String key;
+    public KeysArray keys;
+    public int order = 0;
+    public int drawableId;
+    public boolean allowDisable;
+}
